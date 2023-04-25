a430cab97d7124cf0e241e785da1701140979ce3
FrancoQuiroga <fe.quiroga@alumno.um.edu.ar>
2023-04-24 21:22:57 -0300

    Codigo a casi terminar

--- cambiosdenumeracion.py
+++ cambiosdenumeracion.py
@@ -4,6 +4,44 @@ def binario2decimal(binario):
         total += int(binario[-i-1]) * 2 ** i
 
     return total
+def binario2octal(binario):
+    resultdecimal = binario2decimal(binario)
+    resultoctal = decimal2octal(resultdecimal)
+    return resultoctal
+def binario2hex(binario):
+    resultdecimal = binario2decimal(binario)
+    resulthex = decimal2hex(resultdecimal)
+    return resulthex
+def decimal2octal(decimal):
+    resultoctal = ''
+    resultinvertido = ''
+    while decimal >= 8:
+        resultinvertido += str(decimal % 8)
+        decimal = decimal // 8
+    resultinvertido += str(decimal) 
+    for i in range(0,len(resultinvertido)):
+        resultoctal = resultoctal + resultinvertido[-i-1]
+    return int(resultoctal)
+def decimal2hex(decimal):
+    diccionarioshex = {15: 'F', 14: 'E', 13: 'D', 12: 'C',11: 'B',10: 'A'}
+    valoreshex = (15, 14, 13, 12, 11, 10)
+    resulthex = ''
+    resultinvertido = ''
+    while decimal >= 16:
+        if 16 > (decimal % 16) > 9:
+            for i in valoreshex:
+                if ((decimal % 16) % i) == 0:
+                    resultinvertido += diccionarioshex[i]
+        else:
+            resultinvertido += str(decimal % 16)
+        decimal = decimal // 16
+    if decimal in diccionarioshex:
+        resultinvertido += diccionarioshex[decimal]
+    else:    
+        resultinvertido += str(decimal) 
+    for i in range(0,len(resultinvertido)):
+        resulthex = resulthex + resultinvertido[-i-1]
+    return resulthex
 def decimal2binario(decimal):
     result = ''
     resultinvertido = ''
@@ -16,3 +54,28 @@ def decimal2binario(decimal):
     while len(result) % 4 != 0:
         result = '0' + result
     return result
+def octal2decimal(octal):
+    octalstring = str(octal)
+    totaloctal = 0
+    for i in range(0, len(octalstring)):
+        totaloctal += int(octalstring[-i-1]) * 8 ** i
+    return totaloctal
+def octal2binario(octal):
+    decimalresult = octal2decimal(octal)
+    binarioresult = decimal2binario(decimalresult)
+    return binarioresult
+def octal2hex(octal1):
+    decimalresult = octal2decimal(octal1)
+    hexresult = str(decimal2hex(decimalresult))
+    return hexresult
+def hex2decimal(hex):
+    diccionarioshex = {'F': 15,'E': 14, 'D': 13,'C': 12,'B': 11,'A': 10 }
+    palabraanumero = 0
+    resulthex = 0
+    for i in range(0, len(hex)):
+        if diccionarioshex[i].isnumeric():
+                resulthex += int(hex[-i-1]) * 16 ** i
+        else:
+            palabraanumero = diccionarioshex[i]
+            resulthex += palabraanumero * 16 ** i
+    return resulthex
--- testcambiosdenumeracion.py
+++ testcambiosdenumeracion.py
@@ -1,9 +1,14 @@
 import unittest
 from cambiosdenumeracion import binario2decimal
 from cambiosdenumeracion import decimal2binario
-
-
-
+from cambiosdenumeracion import decimal2octal
+from cambiosdenumeracion import decimal2hex
+from cambiosdenumeracion import binario2hex
+from cambiosdenumeracion import binario2octal
+from cambiosdenumeracion import octal2decimal
+from cambiosdenumeracion import octal2binario
+from cambiosdenumeracion import octal2hex
+from cambiosdenumeracion import hex2decimal
 class Testnumeracion(unittest.TestCase):
 #Test binario a decimal
     def testbinario2decimal(self):
@@ -12,6 +17,16 @@ class Testnumeracion(unittest.TestCase):
         self.assertEqual(binario2decimal('011011011100'), 1756)
     def testbinario2decimal2(self):
         self.assertEqual(binario2decimal('111101011010'), 3930)
+#Test binario a octal
+    def testbinario2octal(self):
+        self.assertEqual(binario2octal('01011100'), 134)
+    def testbinario2octal1(self):
+        self.assertEqual(binario2octal('011011011100'), 3334)
+#Test binario a Hexadecimal
+    def testbinario2hex(self):
+        self.assertEqual(binario2hex('011011011100'), '6DC')
+    def testbinario2hex1(self):
+        self.assertEqual(binario2hex('111011011011100'), '76DC')
 #Test decimal a binario
     def testdecimal2binario(self):
         self.assertEqual(decimal2binario(97), '01100001')
@@ -21,7 +36,45 @@ class Testnumeracion(unittest.TestCase):
         self.assertEqual(decimal2binario(1097), '010001001001')
     def testdecimal2binario3(self):
         self.assertEqual(decimal2binario(10097), '0010011101110001')
+#Test decimal a octal
+    def testdecimal2octal(self):
+        self.assertEqual(decimal2octal(125), 175)
+    def testdecimal2octal2(self):
+        self.assertEqual(decimal2octal(575), 1077)
+    def testdecimal2octal3(self):
+        self.assertEqual(decimal2octal(2789), 5345)
+#Test decimal a Hexadecimal
+    def testdecimal2hex(self):
+        self.assertEqual(decimal2hex(125), '7D')
+    def testdecimal2hex1(self):
+        self.assertEqual(decimal2hex(775), '307')
+    def testdecimal2hex2(self):
+        self.assertEqual(decimal2hex(1903), '76F')
+    def testdecimal2hex3(self):
+        self.assertEqual(decimal2hex(1756), '6DC')
+    def testdecimal2hex4(self):
+        self.assertEqual(decimal2hex(5655), '1617')
+    def testdecimal2hex5(self):
+        self.assertEqual(decimal2hex(3510), 'DB6')
 
+#Test octal a decimal
+    def testoctal2decimal(self):
+        self.assertEqual(octal2decimal(6666), 3510)
+    def testoctal2decimal2(self):
+        self.assertEqual(octal2decimal(4567), 2423)
+    def testoctal2decimal1(self):
+        self.assertEqual(octal2decimal(7654), 4012)
+#Test octal a binario
+    def testoctal2binario(self):
+        self.assertEqual(octal2binario(6666),'110110110110')
+    def testoctal2binario1(self):
+        self.assertEqual(octal2binario(4567),'100101110111')
+#Test octal a hexadecimal
+    def testoctal2hex(self):
+        self.assertEqual(octal2hex(6666), 'DB6')
+#Test Hexadecimal a decimal
+    def testhex2decimal(self):
+        self.assertEqual(hex2decimal('DB6'), 3510)
 if __name__ == '__main__':
     unittest.main()
 
